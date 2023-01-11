9bfaee6f89119ad050c3c07ecfb8d26ce413a3d5
Noheliajoeliana <nohelia.j.r.a@gmail.com>
2023-01-11 19:22:22 -0400

    up: implement r2 upload

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
+const fs = require('fs');
 const core = require('@actions/core');
-const github = require('@actions/github');
+const { S3 } = require('@aws-sdk/client-s3');
+
+const ACCOUNT_ID = core.getInput('account-id');
+const ACCESS_KEY_ID = core.getInput('access-key-id');
+const SECRET_ACCESS_KEY = core.getInput('secret-access-key');
+const BUCKET = core.getInput('r2-bucket');
+const FINAL_PATH = core.getInput('r2-path');
+const FILENAME = core.getInput('filename');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  console.log('Change!!!')
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
+  const client = new S3({
+    region: 'auto',
+    endpoint: `https://${ACCOUNT_ID}.r2.cloudflarestorage.com`,
+    credentials: {
+      accessKeyId: ACCESS_KEY_ID,
+      secretAccessKey: SECRET_ACCESS_KEY
+    }
+  });
+
+  const file = fs.createReadStream(FILENAME);
+  file.on('error', (error) => {
+    core.setFailed(error.message);
+  });
+
+  const destination = FINAL_PATH || FILENAME;
+
+  client.putObject({
+    Body: file,
+    Key: destination,
+    Bucket: BUCKET,
+  }).then(response => {
+    console.log('Success! ', response);
+    core.setOutput('data', response)
+  });
+} catch(error) {
   core.setFailed(error.message);
 }
