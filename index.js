cddfc33762cf0dc70a6b8a6548ca691c3c68a644
Noheliajoeliana <nohelia.j.r.a@gmail.com>
2023-01-16 11:47:36 -0400

    feat: change input name

--- index.js
+++ index.js
@@ -16,60 +16,9 @@ const fileInfo = core.getInput('file-info');
 try {
   const files = JSON.parse(fileInfo);
   if (!Array.isArray(files)) core.setFailed('File info must be an array');
-  //
-  // const client = new S3({
-  //   region: 'auto',
-  //   endpoint: `https://${accountId}.r2.cloudflarestorage.com`,
-  //   credentials: {
-  //     accessKeyId: accessKeyId,
-  //     secretAccessKey: secretAccessKey
-  //   }
-  // });
-  //
+
   const filesToUpload = await matchingFiles(files);
   console.log(filesToUpload);
-  //
-  // function uploadFiles(){
-  //
-  //   const responses = [];
-  //   async function uploadOne(index = 0){
-  //     let { fileName, contentType } = filesToUpload[index];
-  //
-  //     contentType = contentType || mime.lookup(fileName);
-  //
-  //     const stream = fs.createReadStream(fileName);
-  //     stream.on('error', (error) => {
-  //       core.setFailed(error.message);
-  //     });
-  //
-  //     const data = {
-  //       Body: stream,
-  //       Key: finalPath || path.basename(fileName),
-  //       Bucket: bucket,
-  //       ContentType: contentType
-  //     };
-  //
-  //     const response = await client.putObject(data);
-  //     responses.push(response);
-  //
-  //     return (index < files.length - 1) && uploadOne(index + 1);
-  //   }
-  //
-  //   return new Promise(async (resolve, reject) => {
-  //     try {
-  //       await(uploadOne());
-  //       resolve(responses);
-  //     } catch(error) {
-  //       reject(error)
-  //     }
-  //   });
-  //
-  // }
-  //
-  // uploadFiles().then(response => {
-  //   console.log('Success! ', response);
-  //   core.setOutput('data', response)
-  // })
 
 } catch(error) {
   core.setFailed(error.message);
