8621740ed14b3bcdbd84e50731dae37856fe82c6
Noheliajoeliana <nohelia.j.r.a@gmail.com>
2023-01-16 11:56:24 -0400

    fix: await top level

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const { matchingFiles } = require('./utils');
 
 const fileInfo = core.getInput('file-info');
 
+async function execution(){
+  try {
+    const files = JSON.parse(fileInfo);
+    if (!Array.isArray(files)) core.setFailed('File info must be an array');
 
-try {
-  const files = JSON.parse(fileInfo);
-  if (!Array.isArray(files)) core.setFailed('File info must be an array');
+    const filesToUpload = await matchingFiles(files);
+    console.log(filesToUpload);
 
-  const filesToUpload = await matchingFiles(files);
-  console.log(filesToUpload);
-
-} catch(error) {
-  core.setFailed(error.message);
+  } catch(error) {
+    core.setFailed(error.message);
+  }
 }
+execution();
