05896f66ee7202775222cc60885b23ced4ed2ab6
Noheliajoeliana <nohelia.j.r.a@gmail.com>
2023-01-16 11:49:12 -0400

    fix: delete dist

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-const path = require('path');
-const fs = require('fs');
 const core = require('@actions/core');
-const { S3 } = require('@aws-sdk/client-s3');
 const { matchingFiles } = require('./utils');
-const mime = require('mime-types');
 
-const accountId = core.getInput('account-id');
-const accessKeyId = core.getInput('access-key-id');
-const secretAccessKey = core.getInput('secret-access-key');
-const bucket = core.getInput('r2-bucket');
-const finalPath = core.getInput('r2-path');
 const fileInfo = core.getInput('file-info');
 
 
